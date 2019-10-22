1d91315f8d24639d9433aba900ab917495c73e15
Kayzin-Lama25 <kayzinlama25@gmail.com>
2019-10-22 00:59:48 -0700

    commit crisis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'crisis-center', loadChildren: () => import('./crisis-center/crisis-center.module').then(mod => mod.CrisisCenterModule) },
-  { path: 'heroes', loadChildren: () => import('./heroes/hero.module').then(mod => mod.HeroModule) },
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' }
+  /* { path: 'heroes', loadChildren: () => import('./heroes/hero.module').then(mod => mod.HeroModule) },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' } */
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CrisisCenterModule } from './crisis-center/crisis-center.module';
+import { HeroModule } from './heroes/hero.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CrisisCenterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
